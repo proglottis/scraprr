3080173df64c7e3188b848a2cda5754a5277d45d
James Fargher <proglottis@gmail.com>
2013-04-08 08:34:15 +1200

    Shrink public methods

--- lib/scraprr/scraper.rb
+++ lib/scraprr/scraper.rb
@@ -20,41 +20,46 @@ module Scraprr
     def extract
       items = []
       document.search(root_matcher).each do |node|
-        skip = false
-        item = {}
-        attributes.each do |name, opts|
-          next unless opts.has_key?(:matcher)
-          element = node.search(opts[:matcher])
-          value = opts[:html] ?  element.inner_html : element.inner_text
-          if sanitizers.has_key?(name) && value != nil
-            value = sanitizers[name].call(value)
-          end
-          if opts[:required] && (value == nil || value == "")
-            skip = true
-            break
-          end
-          item[name] = value
-        end
-        attributes.each do |name, opts|
-          next unless opts.has_key?(:in)
-          match = opts[:regexp].match(item[opts[:in]])
-          value = match ? match[1] : nil
-          if sanitizers.has_key?(name) && value != nil
-            value = sanitizers[name].call(value)
-          end
-          if opts[:required] && (value == nil || value == "")
-            skip = true
-            break
-          end
-          item[name] = value
-        end
-        attributes.each do |name, opts|
-          next unless opts.has_key?(:composite)
-          item.delete(name)
-        end
-        items << item unless skip
+        item = extract_item(node)
+        items << item unless item[:_skip]
       end
       items
+    end
+
+    private
+
+    def extract_item(node)
+      item = {}
+      attributes.each do |name, opts|
+        next unless opts.has_key?(:matcher)
+        element = node.search(opts[:matcher])
+        value = sanitize(name, opts[:html] ?  element.inner_html : element.inner_text)
+        item[name] = value
+        item[:_skip] = true if skip?(value, opts[:required])
+      end
+      attributes.each do |name, opts|
+        next unless opts.has_key?(:in)
+        match = opts[:regexp].match(item[opts[:in]])
+        value = sanitize(name, match ? match[1] : nil)
+        item[name] = value
+        item[:_skip] = true if skip?(value, opts[:required])
+      end
+      attributes.each do |name, opts|
+        next unless opts.has_key?(:composite)
+        item.delete(name)
+      end
+      item
+    end
+
+    def skip?(value, required)
+      required && (value == nil || value == "")
+    end
+
+    def sanitize(name, value)
+      if sanitizers.has_key?(name) && value != nil
+        value = sanitizers[name].call(value)
+      end
+      value
     end
   end
 end
