8675b4cce3e55bc7e0d426597b695f9c4ac387e5
James Fargher <proglottis@gmail.com>
2013-04-16 11:56:36 +1200

    Refactor object scraper extract

--- lib/scraprr/object_scraper.rb
+++ lib/scraprr/object_scraper.rb
@@ -1,4 +1,6 @@
 module Scraprr
+  class MissingAttributeError < StandardError; end
+
   class ObjectScraper
     attr_reader :attributes
 
@@ -12,18 +14,27 @@ module Scraprr
     end
 
     def extract(node)
-      item = {}
-      attributes.each do |name, opts|
-        element = node.search(opts[:path])
-        value = opts[:html] ?  element.inner_html : element.inner_text
-        if opts.has_key?(:regexp)
-          match = opts[:regexp].match(value)
-          value = match ? match[1] : nil
-        end
-        item[name] = value
-        return nil if opts[:required] && (value == nil || value == "")
+      attributes.reduce({}) do |item, (name, opts)|
+        item[name] = extract_attribute(opts, node)
+        item
       end
-      item
+    rescue MissingAttributeError
+      nil
+    end
+
+    private
+
+    def extract_attribute(opts, node)
+      element = node.search(opts[:path])
+      value = opts[:html] ?  element.inner_html : element.inner_text
+      if opts.has_key?(:regexp)
+        match = opts[:regexp].match(value)
+        value = match ? match[1] : nil
+      end
+      if opts[:required] && (value == nil || value == "")
+        raise MissingAttributeError.new
+      end
+      value
     end
 
   end
